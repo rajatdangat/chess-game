fbb28ca480866e7b17ed192d2afbe769d060f246
rajatdangat <rajatdangat@gmail.com>
2024-05-05 20:09:55 +0530

    feat: initial commit

---
+++ webapp/src/App.tsx
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled";
+
+const StyledText = styled.p`
+  color: red;
+  font-size: 34px;
+`;
+
+function App() {
+  return (
+    <div>
+      <StyledText>Hello World!</StyledText>
+    </div>
+  );
+}
+
+export default App;
