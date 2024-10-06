2d6e7620a484332bc62efab5724d7351d898ff32
rajatdangat <rajatdangat@gmail.com>
2024-10-06 15:17:09 +0530

    feat: randomly move black pieces

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "@emotion/styled";
 import { Chess } from "chess.js";
 
@@ -16,6 +16,21 @@ const squareNotationToIndex = (square: string): [number, number] => {
   const file = square.charCodeAt(0) - 97;
   const rank = Number(square[1]);
   return [8 - rank, file];
+};
+
+const playRandomMove = () => {
+  const possibleMoves = chess.moves({ verbose: true });
+
+  const randomIdx = Math.floor(Math.random() * possibleMoves.length);
+  const move = possibleMoves[randomIdx];
+  chess.move(move.san);
+
+  return [move.from, move.to];
+};
+
+const gameOver = () => {
+  window.alert("Game Over");
+  chess.reset();
 };
 
 export type BoardElement = {
@@ -41,19 +56,29 @@ function App() {
   const [boardHighlights, setBoardHighlights] = useState<BoardElement[]>([]);
   const [boardHints, setBoardHints] = useState<HintElement[]>([]);
 
+  // TODO: game over dialog should display after last move is made
+  useEffect(() => {
+    if (chess.isGameOver()) {
+      gameOver();
+    }
+  }, [board]);
+
+  // TODO: if the piece is unselected then the hints should disappear
   const handleBoardClick = (i: number, j: number) => {
     const piece = board[i][j];
     if (piece !== null) {
       console.log("Clicked on piece!", piece, i, j);
       const moves = chess.moves({ square: piece.square, verbose: true });
       console.log("moves", moves);
-
-      const hints: HintElement[] = [];
-      moves.forEach((move) => {
-        const [rank, file] = squareNotationToIndex(move.to);
-        hints.push({ rank, file, move: move.san, type: "hint" });
-      });
-      setBoardHints(hints);
+      console.log("turn", chess.turn());
+      if (chess.turn() === "w") {
+        const hints: HintElement[] = [];
+        moves.forEach((move) => {
+          const [rank, file] = squareNotationToIndex(move.to);
+          hints.push({ rank, file, move: move.san, type: "hint" });
+        });
+        setBoardHints(hints);
+      }
 
       setBoardHighlights((prev) => {
         const newHighlights = prev.filter((highlight) => highlight.type !== "pieceSelect");
@@ -81,6 +106,27 @@ function App() {
           { rank, file, type: "pieceMoveNewPosition" },
         ]);
       }
+
+      // play black randomly
+      setTimeout(() => {
+        if (chess.turn() === "b") {
+          const [from, to] = playRandomMove();
+          const [fromRank, fromFile] = squareNotationToIndex(from);
+          const [toRank, toFile] = squareNotationToIndex(to);
+
+          setBoard(chess.board());
+          setBoardHighlights((prev) => {
+            const newHighlights = prev.filter(
+              (highlight) => highlight.type !== "pieceMoveOldPosition" && highlight.type !== "pieceMoveNewPosition"
+            );
+            return [
+              ...newHighlights,
+              { rank: fromRank, file: fromFile, type: "pieceMoveOldPosition" },
+              { rank: toRank, file: toFile, type: "pieceMoveNewPosition" },
+            ];
+          });
+        }
+      }, 500);
     } catch (err) {
       console.log(err);
     }
